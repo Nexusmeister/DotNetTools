d1743146732d8e09c8dcee36c5e419f6fc157a15
Kaltenbach, Robin <robin@cskaltenbach.de>
2020-08-20 14:34:31 +0200

    Fixes

--- Datenbanken/ToolsDAT/Tables/Backup/BackupAufgaben.sql
+++ Datenbanken/ToolsDAT/Tables/Backup/BackupAufgaben.sql
@@ -1,19 +1,17 @@
-﻿CREATE TABLE [dbo].[BackupAufgaben](
+﻿CREATE TABLE [dbo].[BackupAufgaben]
+(
 	[Id] [int] IDENTITY(1,1) NOT NULL,
 	[ReihenfolgeNr] [int] NOT NULL,
 	[ZipName] [varchar](100) NOT NULL,
- CONSTRAINT [PK_BackupAufgaben] PRIMARY KEY CLUSTERED 
-(
-	[Id] ASC
-)WITH (STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
+	CONSTRAINT [PK_BackupAufgaben] PRIMARY KEY CLUSTERED (	[Id] ASC )
+)
+
+GO
+EXEC sp_addextendedproperty @name=N'MS_Description', @value=N'AufgabenNr' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'BackupAufgaben', @level2type=N'COLUMN',@level2name=N'Id'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'AufgabenNr' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'BackupAufgaben', @level2type=N'COLUMN',@level2name=N'Id'
+EXEC sp_addextendedproperty @name=N'MS_Description', @value=N'Definiert die Reihenfolge der einzelnen BackupAufgaben' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'BackupAufgaben', @level2type=N'COLUMN',@level2name=N'ReihenfolgeNr'
 GO
 
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'Definiert die Reihenfolge der einzelnen BackupAufgaben' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'BackupAufgaben', @level2type=N'COLUMN',@level2name=N'ReihenfolgeNr'
-GO
-
-EXEC sys.sp_addextendedproperty @name=N'MS_Description', @value=N'[ReihenfolgeNr]_[Bereich].zip' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'BackupAufgaben', @level2type=N'COLUMN',@level2name=N'ZipName'
+EXEC sp_addextendedproperty @name=N'MS_Description', @value=N'[ReihenfolgeNr]_[Bereich].zip' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'BackupAufgaben', @level2type=N'COLUMN',@level2name=N'ZipName'
 GO
--- Datenbanken/ToolsDAT/Tables/Backup/OneDriveBereiche.sql
+++ Datenbanken/ToolsDAT/Tables/Backup/OneDriveBereiche.sql
@@ -1,4 +1,18 @@
 ﻿CREATE TABLE [dbo].[OneDriveBereiche]
 (
-	[Id] INT NOT NULL PRIMARY KEY
+	[Id] [int] IDENTITY(1,1) NOT NULL,
+	[Pfad] [varchar](250) NOT NULL,
+	[AufgabenNr] [int] NOT NULL,
+ CONSTRAINT [PK_OneDriveBereiche] PRIMARY KEY CLUSTERED (	[Id] ASC ), 
+    CONSTRAINT [FK_OneDriveBereiche_BackupAufgaben] FOREIGN KEY (AufgabenNr) REFERENCES BackupAufgaben(Id)
 )
+GO
+
+EXEC sp_addextendedproperty @name=N'MS_Description', @value=N'BereichsNr' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'OneDriveBereiche', @level2type=N'COLUMN',@level2name=N'Id'
+GO
+
+EXEC sp_addextendedproperty @name=N'MS_Description', @value=N'Pfad des Bereichs' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'OneDriveBereiche', @level2type=N'COLUMN',@level2name=N'Pfad'
+GO
+
+EXEC sp_addextendedproperty @name=N'MS_Description', @value=N'Fremdschlüssel auf Tabelle BackupAufgaben' , @level0type=N'SCHEMA',@level0name=N'dbo', @level1type=N'TABLE',@level1name=N'OneDriveBereiche', @level2type=N'COLUMN',@level2name=N'AufgabenNr'
+GO
