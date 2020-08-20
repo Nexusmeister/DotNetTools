e02e94a588b684b579a0b93d0cf8d313d22ac7f2
Kaltenbach, Robin <robin@cskaltenbach.de>
2020-08-20 14:47:42 +0200

    Neue Tabelle StundenErfassungen

---
+++ Datenbanken/ToolsDAT/Tables/TimeTracker/StundenErfassungen.sql
@@ -0,0 +1,65 @@
+﻿CREATE TABLE [dbo].[StundenErfassungen]
+(
+	[Id] INT IDENTITY(1,1) NOT NULL, 
+    [ErfassungsArt] INT NOT NULL, 
+    [Erfassungsdatum] DATE NOT NULL DEFAULT GETDATE(), 
+    [BeginnArbeit] DATETIME NOT NULL DEFAULT GETDATE(), 
+    [EndeArbeit] DATETIME NULL, 
+    [Notizen] VARCHAR(1000) NULL, 
+    CONSTRAINT [PK_StundenErfassungen] PRIMARY KEY (Id) 
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Erfassungstag',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'StundenErfassungen',
+    @level2type = N'COLUMN',
+    @level2name = N'Erfassungsdatum'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'PK der Tabelle',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'StundenErfassungen',
+    @level2type = N'COLUMN',
+    @level2name = N'Id'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Start der Arbeit',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'StundenErfassungen',
+    @level2type = N'COLUMN',
+    @level2name = N'BeginnArbeit'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Ende der Arbeitszeit',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'StundenErfassungen',
+    @level2type = N'COLUMN',
+    @level2name = N'EndeArbeit'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Zusätzliche Notizen zum Arbeitstag',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'StundenErfassungen',
+    @level2type = N'COLUMN',
+    @level2name = N'Notizen'
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Welche Erfassung wurde aufgenommen - Siehe Tabelle Status',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'StundenErfassungen',
+    @level2type = N'COLUMN',
+    @level2name = N'ErfassungsArt'
