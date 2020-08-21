3bf5de3c6c5e4a4792bf3cfa357f8993f8a3f07e
Kaltenbach, Robin <robin@cskaltenbach.de>
2020-08-21 22:54:47 +0200

    Datenzugriff für StundenErfassung hinzugefügt

---
+++ DLLs/Tools.Core/Datenzugriff/Repositories/Tools/TimeTracker/Interfaces/IStundenErfassungRepository.cs
@@ -0,0 +1,10 @@
+﻿using Tools.Core.Datenzugriff.Repositories.BaseRepositories;
+using Tools.Core.Models.Tools.TimeTracker;
+
+namespace Tools.Core.Datenzugriff.Repositories.Tools.TimeTracker.Interfaces
+{
+    public interface IStundenErfassungRepository : IEditableRepository<StundenErfassung>
+    {
+        
+    }
+}
