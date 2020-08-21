4eb8e05162e77d3a6072f780048a98549fd74392
Kaltenbach, Robin <robin@cskaltenbach.de>
2020-08-21 20:58:50 +0200

    Repository Pattern aufgebaut

---
+++ DLLs/Tools.Core/Datenzugriff/Repositories/Administration/Interfaces/ICustomizingRepository.cs
@@ -0,0 +1,10 @@
+﻿using Tools.Core.Datenzugriff.Repositories.BaseRepositories;
+using Tools.Core.Models.Administration;
+
+namespace Tools.Core.Datenzugriff.Repositories.Administration.Interfaces
+{
+    public interface ICustomizingRepository : IBaseRepository<Customizing>
+    {
+        
+    }
+}
