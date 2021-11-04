85b7f065092a5dbb10438b7e67898cd2c0415788
Kaltenbach, Robin <robin@cskaltenbach.de>
2021-11-04 23:42:30 +0100

    Neues Tool, um Files rekursiv zu moven

---
+++ DLLs/Tools.Core/Tools/Configuration/FileMoverConfig.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
+
+namespace Tools.Core.Tools.Configuration
+{
+    public class FileMoverConfig : IConfigurable<FileMoverConfig>
+    {
+        public string QuellOrdner { get; set; }
+        public string ZielOrdner { get; set; }
+
+        [JsonIgnore]
+        public Exception Error { get; set; }
+
+        public FileMoverConfig GetConfig()
+        {
+            try
+            {
+                var configPfad = AppContext.BaseDirectory + @"\Config.json";
+                if (!File.Exists(configPfad))
+                {
+                    var jsonToCreate = JsonConvert.SerializeObject(this);
+                    using (var sw = new StreamWriter(configPfad, false))
+                    {
+                        sw.Write(jsonToCreate);
+                        sw.Close();
+                    }
+                }
+
+                var json = File.ReadAllText(configPfad);
+                return JsonConvert.DeserializeObject<FileMoverConfig>(json);
+            }
+            catch (Exception e)
+            {
+                return new FileMoverConfig
+                {
+                    Error = e
+                };
+            }
+        }
+    }
+}
---
+++ Tools/FileMover/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using Tools.Core.Tools;
+using Tools.Core.Tools.Configuration;
+
+namespace FileMover
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new FileMoverConfig().GetConfig();
+            var logger = new ToolsLogger();
+            if (string.IsNullOrWhiteSpace(config.QuellOrdner) || string.IsNullOrWhiteSpace(config.ZielOrdner))
+            {
+                Environment.Exit(1);
+            }
+
+            DirectoryCopy(config.QuellOrdner, config.ZielOrdner, true);
+        }
+
+        private static void DirectoryCopy(string quellPfad, string zielPfad, bool copySubDirs)
+        {
+            // Get the subdirectories for the specified directory.
+            var quelle = new DirectoryInfo(quellPfad);
+
+            if (!quelle.Exists)
+            {
+                throw new DirectoryNotFoundException(
+                    "Source directory does not exist or could not be found: "
+                    + quellPfad);
+            }
+
+            var dirs = quelle.EnumerateDirectories();
+
+            // If the destination directory doesn't exist, create it.       
+            if (!Directory.Exists(zielPfad))
+            {
+                Directory.CreateDirectory(zielPfad);
+            }
+
+            // Get the files in the directory and copy them to the new location.
+            var files = quelle.EnumerateFiles();
+            foreach (FileInfo file in files)
+            {
+                string tempPath = Path.Combine(zielPfad, file.Name);
+                file.CopyTo(tempPath, true);
+            }
+
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string tempPath = Path.Combine(zielPfad, subdir.Name);
+                    DirectoryCopy(subdir.FullName, tempPath, copySubDirs);
+                }
+            }
+        }
+    }
+}
